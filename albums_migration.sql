84f1cbd5b576a006b3b907dfce6a6accf2edcc2a
Orenthal Hill <ojhill98@gmail.com>
2021-10-01 15:40:51 -0500

    update albums_migration

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
                        id INT UNSIGNED AUTO_INCREMENT,
-                       artist VARCHAR(35) NOT NULL,
-                       name VARCHAR(65) NOT NULL,
-                       release_date DATETIME NOT NULL,
-                       sales DOUBLE NOT NULL DEFAULT 0,
-                       genre VARCHAR(35) NOT NULL,
+                       artist VARCHAR(128) NOT NULL,
+                       name VARCHAR(128) NOT NULL,
+                       release_date SMALLINT NOT NULL,
+                       sales FLOAT (6,2) NOT NULL DEFAULT 0,
+                       genre VARCHAR(128) NOT NULL,
                        PRIMARY KEY (id)
 );
